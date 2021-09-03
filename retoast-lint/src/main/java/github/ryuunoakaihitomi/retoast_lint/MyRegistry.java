53703a280dc1d694a63e3ebc8edcd4f6ca3d6c84
ryuunoakaihitomi <22442525+ryuunoakaihitomi@users.noreply.github.com>
2021-09-03 17:46:11 +0800

    Release version 1.7.4
    lint
    [Fix] Warning: "class-MyRegistry in path-to-lint.jar does not specify a vendor; see IssueRegistry#vendor"

--- retoast-lint/src/main/java/github/ryuunoakaihitomi/retoast_lint/MyRegistry.java
+++ retoast-lint/src/main/java/github/ryuunoakaihitomi/retoast_lint/MyRegistry.java
@@ -1,16 +1,24 @@
 package github.ryuunoakaihitomi.retoast_lint;
 
 import com.android.tools.lint.client.api.IssueRegistry;
+import com.android.tools.lint.client.api.Vendor;
 import com.android.tools.lint.detector.api.ApiKt;
 import com.android.tools.lint.detector.api.Issue;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.List;
 
 @SuppressWarnings("UnstableApiUsage")
 public class MyRegistry extends IssueRegistry {
+
+    @Nullable
+    @Override
+    public Vendor getVendor() {
+        return new Vendor("ZQY", null, "https://github.com/ryuunoakaihitomi/ReToast/issues");
+    }
 
     @Override
     public int getApi() {
